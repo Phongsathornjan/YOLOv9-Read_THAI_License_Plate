f9519ae11e916e1e82e164bda29d5e44f0196fad
phongsathornjanjamsai@gmail.com <phongsathornjanjamsai@gmail.com>
2025-01-16 09:07:26 +0700

    add Time count

--- services/readLicensePlate.py
+++ services/readLicensePlate.py
@@ -3,16 +3,25 @@ from flask import jsonify
 from services.handleImage import handleImage
 from services.cropLicensePlate import cropLicensePlate
 from services.yoloRead import yoloRead
+import time
 def readLicensePlate(request):
     try:
+        
+        start_time = time.time()
+        
         handleImage(request) #save image
         
         #crop image
         crop_result = cropLicensePlate()
         if len(crop_result) == 0:
-            return jsonify({'massage':"can't detect license plate"})
+            return jsonify({'message':"can't detect license plate"})
         
         result = yoloRead()
+        
+        end_time = time.time()
+        process_time = end_time - start_time
+        print("time : "+str(process_time))
+        
         return jsonify(result)
         
     except Exception as e:
